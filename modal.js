1fdbba204074cc9b9c85610bcfd0ab22e7a08f01
KarimB1980 <96720369+KarimB1980@users.noreply.github.com>
2022-09-10 12:34:01 +0200

    Ajout d'une bordure rouge aux inputs lors de l'affichage d'un message d'erreur

--- modal.js
+++ modal.js
@@ -43,10 +43,17 @@ function controleFormulaire() {
   document.getElementById('first').onchange = function () {
     const firstName = document.getElementById('first').value;
     if (/^[a-zA-Z \-]{2,100}$/.test(firstName)) {
+      // Masquage du coutour de couleur rouge du champ "Prénom"
+      document.getElementById('first').style.border="0px solid #F05282";
+      // Effacement du message d'erreur
       document.querySelector("#firstMessageErreur").innerHTML = "";
       return true;
     } else {
+      // Effacement de la valeur fausse
       document.getElementById('first').value = "";
+      // Coutour de couleur rouge du champ "Prénom"
+      document.getElementById('first').style.border="2px solid #F05282";
+      // Affichage du message d'erreur sous le champ "Prénom"
       document.querySelector("#firstMessageErreur").innerHTML = "Veuillez renseigner un prénom valide (ne comportant que des minuscules, majuscules, tirets, espaces et 2 à 100 caractères maximum).";
     }
   }
@@ -55,10 +62,17 @@ function controleFormulaire() {
   document.getElementById('last').onchange = function () {
     const lastName = document.getElementById('last').value;
     if (/^[a-zA-Z \-]{2,100}$/.test(lastName)) {
+      // Masquage du coutour de couleur rouge du champ "Nom"
+      document.getElementById('last').style.border="0px solid #F05282";
+      // Effacement du message d'erreur
       document.querySelector("#lastMessageErreur").innerHTML = "";
       return true;
     } else {
+      // Effacement de la valeur fausse
       document.getElementById('last').value = "";
+      // Coutour de couleur rouge du champ "Nom"
+      document.getElementById('last').style.border="2px solid #F05282";
+      // Affichage du message d'erreur sous le champ "Nom"
       document.querySelector("#lastMessageErreur").innerHTML = "Veuillez renseigner un nom valide (ne comportant que des minuscules, majuscules, tirets, espaces et 2 à 100 caractères maximum).";
     }
   }
@@ -67,10 +81,17 @@ function controleFormulaire() {
   document.getElementById('email').onchange = function () {
     const Email = document.getElementById('email').value;
     if (/^[\w\.]+@([\w]+\.)+[\w]{2,4}$/.test(Email)) {
+      // Masquage du coutour de couleur rouge du champ "E-mail"
+      document.getElementById('email').style.border="0px solid #F05282";
+      // Effacement du message d'erreur
       document.querySelector("#emailMessageErreur").innerHTML = "";
       return true;
     } else {
+      // Effacement de la valeur fausse
       document.getElementById('email').value = "";
+      // Coutour de couleur rouge du champ "E-mail"
+      document.getElementById('email').style.border="2px solid #F05282";
+      // Affichage du message d'erreur sous le champ "E-mail"
       document.querySelector("#emailMessageErreur").innerHTML = "Veuillez renseigner un email valide (respectant le format xxxx@xxx.xxx avec une terminaison de 1 à 4 caractères).";
     }
   }
@@ -79,10 +100,17 @@ function controleFormulaire() {
   document.getElementById('birthdate').onchange = function () {
     const dateNaissance = document.getElementById('birthdate').value;
     if (/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(Email)) {
+      // Masquage du coutour de couleur rouge du champ "Date de naissance"
+      document.getElementById('birthdate').style.border="0px solid #F05282";
+      // Effacement du message d'erreur
       document.querySelector("#naissanceMessageErreur").innerHTML = "";
       return true;
     } else {
+      // Effacement de la valeur fausse
       document.getElementById('birthdate').value = "";
+      // Coutour de couleur rouge du champ "Date de naissance"
+      document.getElementById('birthdate').style.border="2px solid #F05282";
+      // Affichage du message d'erreur sous le champ "Date de naissance"
       document.querySelector("#naissanceMessageErreur").innerHTML = "Veuillez renseigner une date valide (respectant le format jj/mm/aaaa).";
     }
   }
@@ -91,10 +119,17 @@ function controleFormulaire() {
   document.getElementById('quantity').onchange = function () {
     const nombreTournois = document.getElementById('quantity').value;
     if (/^[1-9]{0,1}[0-9]$/.test(nombreTournois)) {
+      // Masquage du coutour de couleur rouge du champ "À combien de tournois GameOn avez-vous déjà participé ?"
+      document.getElementById('quantity').style.border="0px solid #F05282";
+      // Effacement du message d'erreur
       document.querySelector("#nombretournoisMessageErreur").innerHTML = "";
       return true;
     } else {
+      // Effacement de la valeur fausse
       document.getElementById('quantity').value = "";
+      // Coutour de couleur rouge du champ "À combien de tournois GameOn avez-vous déjà participé ?"
+      document.getElementById('quantity').style.border="2px solid #F05282";
+      // Affichage du message d'erreur sous le champ "À combien de tournois GameOn avez-vous déjà participé ?"
       document.querySelector("#nombretournoisMessageErreur").innerHTML = "Veuillez saisir une valeur comprise entre 0 inclus et 99 inclus";
     }
   }
